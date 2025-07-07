2a298da5525d6fb5bccc597ac3ce36e30ba960de
Andrey Angos <andreyangos123@gmail.com>
2025-07-06 19:54:22 -0700

    Added median function to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
+import math
+
+def median(input):
+  input.sort()
+  if len(input) % 2 == 0:
+    return (input[math.floor(len(input) / 2)] + input[math.floor(len(input) / 2) - 1]) / 2
+  else:
+    return input[math.floor(len(input) / 2)]
+  
 print("hello_world")
